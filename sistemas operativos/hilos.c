96179739e294ae096796dd33cbeb0068aab720d5
Santos_Arellano <Arellanosantoso6@gmail.com>
2023-11-21 10:11:28 -0600

    hilos

--- sistemas operativos/hilos.c
+++ sistemas operativos/hilos.c
@@ -1,21 +1,24 @@
-#include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <pthread.h>
+int saldo;
 
-int saldo = 1000; // Un saldo de ejemplo
-
-void *incrementoSaldo() {
-    printf("El saldo actual es %d\n", saldo);
+void * holaHilo(){  
+    printf("Hola desde hiloo \n");
     pthread_exit(NULL);
 }
 
-int main() {
-    pthread_t t;
+void* Incrementa(){
+    printf("Saldo actual es: %d\n", saldo);
+    saldo = saldo + 100;
+}
 
-    // Crear un hilo para ejecutar incrementoSaldo
-    pthread_create(&t, NULL, incrementoSaldo, NULL);
-
-    // Esperar a que el hilo termine
-    pthread_join(t, NULL);
-
-    return 0;
+int main(){
+    saldo=0;
+    for(int i = 0; i<=100 ;i++){
+        pthread_t t;
+        Incrementa();
+        pthread_create(&t, NULL, holaHilo, NULL); 
+    }
+    pthread_exit(NULL);
 }
